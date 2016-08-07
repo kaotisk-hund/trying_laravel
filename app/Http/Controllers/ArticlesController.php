05eebb032035a267890cc68753582630697823e1
kaotisk <ccc2007chaos@gmail.com>
2016-08-07 18:25:51 +0300

    Comments

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,31 +7,54 @@ use App\Articles;
 use Request;
 
 
+/**
+ * Class ArticlesController
+ * @package App\Http\Controllers
+ */
 class ArticlesController extends Controller
 {
+    /**
+     * Shows all articles
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
-        $articles = Articles::latest()->get();
+        $articles = Articles::latest()->published()->get();
         return view('articles.index', compact('articles'));
     }
 
+
+    /**
+     * Shows selected article
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($id)
     {
         $article = Articles::findOrFail($id);
         return view('articles.show', compact('article'));
     }
 
+    /**
+     * Creates a new article
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view ('articles.create');
     }
 
+    /**
+     * Store the created article
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store()
     {
-
-        $input = Request::all();
-        Articles::create($input);
-
+        Articles::create(Request::all());
         return redirect('articles');
     }
 }
