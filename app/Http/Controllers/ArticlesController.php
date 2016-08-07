ee49f5c34c0b9f711d7846a0420c84d866486ed0
kaotisk <ccc2007chaos@gmail.com>
2016-08-07 19:23:21 +0300

    Added edit() and update() functions.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -58,4 +58,31 @@ class ArticlesController extends Controller
         Articles::create($request->all());
         return redirect('articles');
     }
+
+
+    /**
+     * Edits existing article.
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $article = Articles::findOrFail($id);
+        return view('articles.edit', compact('article'));
+    }
+
+    /**
+     * Updates the edited article.
+     *
+     * @param $id
+     * @param CreateArticleRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, CreateArticleRequest $request)
+    {
+        $article = Articles::findOrFail($id);
+        $article->update($request->all());
+        return redirect('articles');
+    }
 }
