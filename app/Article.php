18542e8cd92e4d99bed0086f6c4078e6b64871e2
kaotisk <ccc2007chaos@gmail.com>
2016-08-08 01:58:55 +0300

    Added relationship has-many articles to user. Also, renamed model Articles to Article.

--- app/Article.php
+++ app/Article.php
@@ -5,28 +5,57 @@ namespace App;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
-class Articles extends Model
+class Article extends Model
 {
+    /**
+     * Fields allowed to mass assign.
+     *
+     * @var array
+     */
     protected $fillable = [
     	'title',
     	'body',
         'published_at'
     ];
 
-    protected $dates = ['published_at'];
+    /**
+     * Carbon based date fields.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'published_at'
+    ];
 
+    /**
+     * Returns Published articles.
+     *
+     * @param $query
+     */
     public function scopePublished($query)
     {
         $query->where('published_at', '<=', Carbon::now());
     }
 
+    /**
+     * Returns Unpublished articles.
+     *
+     * @param $query
+     */
     public function scopeUnpublished($query)
     {
         $query->where('published_at', '>', Carbon::now());
     }
 
+    /**
+     * Sets the published_at attribute as Carbon parsed $date.
+     *
+     * @param $date
+     */
     public function setPublishedAtAttribute($date)
     {
         $this->attributes['published_at'] = Carbon::parse($date);
     }
+
+
 }
