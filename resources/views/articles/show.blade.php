ed9dfe3aba56920f3277804b9cee74bbb724ec1f
kaotisk <ccc2007chaos@gmail.com>
2016-08-12 00:36:31 +0300

    Added edit button.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <h1>{{ $article->title }}</h1>
 
+    @unless(Auth::guest())
+        <h4><a href="{{ url('/articles/'.$article->id.'/edit') }}">Edit</a></h4>
+    @endunless
+
     <div class="body">{{ $article->body }}</div>
 
 @stop
