d2d52bf22ec29e79c52b5536e4c5e468eeae06a4
kaotisk <ccc2007chaos@gmail.com>
2016-08-11 22:19:14 +0300

    Removed css classes from element.

--- resources/views/partials/radio.blade.php
+++ resources/views/partials/radio.blade.php
@@ -1,4 +1,4 @@
-<audio class="pull-right media-bottom" controls>
+<audio controls>
     <source src="http://127.0.0.1:8000/kaotisk" type="audio/ogg">
     Your browser does not support the audio element.
 </audio>
