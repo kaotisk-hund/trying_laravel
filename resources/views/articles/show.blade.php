8f153114e5b7d149eacd552710e8f38eeeda3681
kaotisk <ccc2007chaos@gmail.com>
2016-08-13 06:58:15 +0300

    Insert tags into show article template.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,9 +4,17 @@
     <h1>{{ $article->title }}</h1>
 
     @unless(Auth::guest())
-        <h4><a href="{{ url('/articles/'.$article->id.'/edit') }}">Edit</a></h4>
+        <h4><a class="btn btn-primary" href="{{ url('/articles/'.$article->id.'/edit') }}">Edit</a></h4>
     @endunless
-
+    <div class="tags">{{ $article->tag }}</div>
     <div class="body">{{ $article->body }}</div>
 
+    @unless($article->tags->isEmpty())
+        <h5>Tags:</h5>
+        <ul>
+            @foreach( $article->tags as $tag )
+                {{ $tag->name }}
+            @endforeach
+        </ul>
+    @endunless
 @stop
