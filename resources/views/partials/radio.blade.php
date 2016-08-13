fd46cbe14f6386f215a2f40b0e5080351ad6653a
kaotisk <ccc2007chaos@gmail.com>
2016-08-13 06:56:21 +0300

    Fixed style.

--- resources/views/partials/radio.blade.php
+++ resources/views/partials/radio.blade.php
@@ -1,4 +1,10 @@
-<audio controls>
-    <source src="http://127.0.0.1:8000/kaotisk" type="audio/ogg">
-    Your browser does not support the audio element.
-</audio>
+<div class="panel panel-default">
+    <div class="panel-heading">Radio</div>
+
+    <div class="panel-body">
+        <audio controls>
+            <source src="http://127.0.0.1:8000/kaotisk" type="audio/ogg">
+            Your browser does not support the audio element.
+        </audio>
+    </div>
+</div>
