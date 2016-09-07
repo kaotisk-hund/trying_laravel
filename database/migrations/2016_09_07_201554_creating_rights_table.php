8d6cce0ab77b9d6894090dd2ec3f01a8f30b1df2
kaotisk <ccc2007chaos@gmail.com>
2016-09-07 20:19:46 +0300

    New migration, rights.

---
+++ database/migrations/2016_09_07_201554_creating_rights_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatingRightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('group');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rights');
+    }
+}
